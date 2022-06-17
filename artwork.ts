fdb646ded60b4e88f461ea18afd51ca619d0ad4e
Anton Matyunin <63484644+goal-shevchenko@users.noreply.github.com>
2022-06-17 15:48:07 +0800

    Add files via upload

---
+++ artwork.ts
@@ -0,0 +1,77 @@
+import { Entity } from './entity';
+import { injectable } from 'inversify';
+import {
+  IArtworkEntity,
+  ICyoStatus,
+  IContact,
+  IThumbnail,
+  IPublished
+} from './types';
+
+// Collection: artwork
+@injectable()
+export class Artwork extends Entity<IArtworkEntity> {
+  constructor(props: IArtworkEntity) {
+    super(props);
+  }
+
+  /**
+   * Creates CYO gallery entity
+   * @param props Artwork properties
+   * @returns Artwork
+   */
+  public static create(props: IArtworkEntity): Artwork {
+    const instance = new Artwork(props);
+    return instance;
+  }
+
+  get id(): string {
+    return this._props.id || '';
+  }
+
+  /**
+   * Gets img url
+   */
+  get imgUrl(): string {
+    return this.props.imgUrl || '';
+  }
+
+  /**
+   * Gets thumbnail objects
+   */
+  get thumbnails(): IThumbnail | undefined {
+    return this.props.thumbnails;
+  }
+
+  /**
+   * Author of CYO
+   */
+  get createdBy(): string | undefined {
+    return this.props.createdBy;
+  }
+
+  /**
+   * Personal message of CYO owner
+   */
+  get message(): string | undefined {
+    return this.props.message;
+  }
+
+  /**
+   * Gets contact
+   */
+  get contact(): Partial<IContact> {
+    return this.props.contact;
+  }
+
+  /**
+   * Gets status
+   */
+  get status(): ICyoStatus {
+    return this.props.status;
+  }
+
+  get publishedAt(): IPublished {
+    return this.props.publishedAt;
+  }
+}
